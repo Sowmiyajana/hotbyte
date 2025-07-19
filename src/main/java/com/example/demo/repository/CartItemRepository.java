55612f9dd56c21c57d6ba46b0e9f34a621ba4938
Sowmiyajana <sowmiyajana14@gmail.com>
2025-07-19 18:01:48 +0530

    varun

---
+++ src/main/java/com/example/demo/repository/CartItemRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+
+import com.example.demo.model.CartItem;
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByUser(User user);
+    void deleteByUser(User user);
+}
